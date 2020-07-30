a490118dc8b8b0dfed44e48296553b82804ecde2
luca <yangshaoxiong5545@gmail.com>
2020-07-30 21:13:31 +0800

    project init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBrnBpjysSvlWuNYqK7jzt0isS15EszCO4",
+    authDomain: "clone-78f83.firebaseapp.com",
+    databaseURL: "https://clone-78f83.firebaseio.com",
+    projectId: "clone-78f83",
+    storageBucket: "clone-78f83.appspot.com",
+    messagingSenderId: "473901471194",
+    appId: "1:473901471194:web:cc2509b73381f188ea5ce3",
+    measurementId: "G-TCL17Z9WD6"
+};
