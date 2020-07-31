a9e400009b331cc172654aab5a1930a047559d7f
luca <yangshaoxiong5545@gmail.com>
2020-08-01 00:17:57 +0800

    feat: build recommended videos

--- src/components/RecommendedVideos/index.js
+++ src/components/RecommendedVideos/index.js
@@ -1,10 +1,77 @@
 import React from 'react';
 import './index.css';
+import VideoCard from '../VideoCard';
 
 function RecommendedVideos() {
     return (
         <div className="recommendedVideos">
-            RecommendedVideos
+            <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                <VideoCard 
+                    title="Doomfist Community Montage"
+                    views="2.3M Views"
+                    timestamp="3 days age"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Luca Young"
+                    image="https://i.ytimg.com/vi/TDXC1ztCakU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBxt7ap2X10CobbRVFWGGnEvheioQ"
+                />
+                <VideoCard 
+                    title="Doomfist Community Montage"
+                    views="2.3M Views"
+                    timestamp="3 days age"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Luca Young"
+                    image="https://i.ytimg.com/vi/TDXC1ztCakU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBxt7ap2X10CobbRVFWGGnEvheioQ"
+                />
+                <VideoCard 
+                    title="Doomfist Community Montage"
+                    views="2.3M Views"
+                    timestamp="3 days age"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Luca Young"
+                    image="https://i.ytimg.com/vi/TDXC1ztCakU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBxt7ap2X10CobbRVFWGGnEvheioQ"
+                />
+                <VideoCard 
+                    title="Doomfist Community Montage"
+                    views="2.3M Views"
+                    timestamp="3 days age"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Luca Young"
+                    image="https://i.ytimg.com/vi/TDXC1ztCakU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBxt7ap2X10CobbRVFWGGnEvheioQ"
+                />
+                <VideoCard 
+                    title="Doomfist Community Montage"
+                    views="2.3M Views"
+                    timestamp="3 days age"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Luca Young"
+                    image="https://i.ytimg.com/vi/TDXC1ztCakU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBxt7ap2X10CobbRVFWGGnEvheioQ"
+                />
+                <VideoCard 
+                    title="Doomfist Community Montage"
+                    views="2.3M Views"
+                    timestamp="3 days age"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Luca Young"
+                    image="https://i.ytimg.com/vi/TDXC1ztCakU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBxt7ap2X10CobbRVFWGGnEvheioQ"
+                />
+                <VideoCard 
+                    title="Doomfist Community Montage"
+                    views="2.3M Views"
+                    timestamp="3 days age"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Luca Young"
+                    image="https://i.ytimg.com/vi/TDXC1ztCakU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBxt7ap2X10CobbRVFWGGnEvheioQ"
+                />
+                <VideoCard 
+                    title="Doomfist Community Montage"
+                    views="2.3M Views"
+                    timestamp="3 days age"
+                    channelImage="https://yt3.ggpht.com/a-/AOh14GhOUP_9re93yOMdN4RLQLHl6Mbk6vmUS1524w=s68-c-k-c0x00ffffff-no-rj-mo"
+                    channel="Luca Young"
+                    image="https://i.ytimg.com/vi/TDXC1ztCakU/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLBxt7ap2X10CobbRVFWGGnEvheioQ"
+                />   
+            </div>
         </div>
     )
 }
